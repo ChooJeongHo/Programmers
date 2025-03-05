98c8518d5dcc5049f537f70bcc28ba2f9f131045
Jeong Ho Choo <kd64choo@gmail.com>
2025-03-05 19:21:19 +0900

    [level 0] Title: 배열 두배 만들기, Time: 15.94 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        for (i in numbers) {
+            answer += i * 2
+        }
+        return answer
+    }
+}
